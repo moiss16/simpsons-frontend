351cc53706b0bee438446424ba8b5a413511ad0e
Moises Alberto Rodriguez Aceves <mr@MacBook-Pro-de-Moises.local>
2019-10-15 21:58:12 -0700

    creacion del repositorio

---
+++ app.js
@@ -0,0 +1,21 @@
+const Container = document.getElementById('container');
+const apiUrl = 'https://localhost:5001/simpsons/Character';
+
+const getsimpsonsData = async() =>{
+     const response = await fetch(apiUrl);
+     console.log(response);
+     const simpsonsData = await response.json();
+     console.log(simpsonsData);
+     //const { firstname, lastname, age } = simpsonsData;
+     simpsonsData.forEach(element => {
+          const { firstName, lastName, age, description } = element;
+          Container.innerHTML  += `<div id="simpson-name">${firstName}</div>
+          <div id="simpson-last">${lastName}</div>
+          <div id="simpson-age">${age}</div>
+          <div id="simpson-description">${description}</div>`
+     });
+     
+};
+
+
+getsimpsonsData();
